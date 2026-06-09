05b9f7032c89a14b8a351e2a438531fc7cd0aa29
Dkolomy <Dkolomy@hotmail.com>
2026-06-09 15:52:11 -0400

    Implement random fruit selection and list manipulation in day004.py

---
+++ src/day-005/day005.py
@@ -0,0 +1,38 @@
+# fruits = ["Apple", "Banana", "Cherry"]
+# for fruit in fruits:
+#   print(fruit)
+
+sudent_scores = [78, 65, 89, 86, 55, 91, 64, 89]
+# print(sum(sudent_scores))
+
+# sudent_scores_sum = 0
+# for score in sudent_scores:
+#     sudent_scores_sum += score
+# print(sudent_scores_sum)
+
+# max_score = max(sudent_scores)
+# print(max_score)
+
+# student_scores_max = 0
+# for score in sudent_scores:
+#     if score > student_scores_max:
+#         student_scores_max = score
+# print(student_scores_max)
+
+# student_scores_min = 100
+# for score in sudent_scores:
+#     if score < student_scores_min:
+#         student_scores_min = score
+# print(student_scores_min)
+
+# student_scores_avg = sum(sudent_scores) / len(sudent_scores)
+# print(student_scores_avg)
+
+for number in range(1, 11, 2):
+  print(number)
+
+# The Gause Challenge
+total = 0
+for number in range(1, 101):
+  total += number
+print(total)
---
+++ src/day-005/password_generator.py
@@ -0,0 +1,28 @@
+import random
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+password = ""
+for i in range(nr_letters):
+  password += random.choice(letters)
+for i in range(nr_symbols):
+  password += random.choice(symbols)
+for i in range(nr_numbers):
+  password += random.choice(numbers)
+
+print(password)
+
+# Shuffle the password
+password_list = list[str](password)
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+password = "".join(password_list)
+print(password) 
