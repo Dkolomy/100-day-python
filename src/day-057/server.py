8091fcede846ad55078477c16dbfa2b4f144b2d9
Dkolomy <Dkolomy@hotmail.com>
2026-07-30 17:28:56 -0400

    Refactor Selenium automation in main.py to streamline gym class booking process and enhance verification methods, building on previous improvements.

---
+++ src/day-057/server.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+import random
+from datetime import datetime
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', random_number=random.randint(1, 100), year=datetime.now().year)
+
+@app.route('/guess/<name>')
+def guess(name):
+  gender_url = f"https://api.genderize.io?name={name}"
+  gender_response = requests.get(gender_url)
+  gender_data = gender_response.json()
+  gender = gender_data['gender']
+  age_url = f"https://api.agify.io?name={name}"
+  age_response = requests.get(age_url)
+  age_data = age_response.json()
+  age = age_data['age']
+  return render_template('guess.html', person_name=name, gender=gender, age=age)
+  
+
+@app.route('/blog')
+def get_blog():
+  return render_template('blog.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
